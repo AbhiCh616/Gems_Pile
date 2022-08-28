d98511fa670eabb05a1c43c48e0f89323ed838f2
abhic <abhich1607@gmail.com>
2022-08-28 23:18:49 +0530

    Url text field and save button working

--- app/src/main/java/com/example/gemspile/ui/add_video/AddVideoFragment.kt
+++ app/src/main/java/com/example/gemspile/ui/add_video/AddVideoFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.gemspile.R
 import com.example.gemspile.databinding.AddVideoBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AddVideoFragment : Fragment() {
     private val viewModel: AddVideoViewModel by viewModels()
     private lateinit var binding: AddVideoBinding
@@ -19,5 +24,29 @@ class AddVideoFragment : Fragment() {
     ): View {
         binding = AddVideoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.urlTextField.let { editTextField ->
+            editTextField.setText(viewModel.urlTextField)
+            editTextField.doAfterTextChanged { editable ->
+                editable?.let {
+                    viewModel.urlTextField = editable.toString()
+                }
+            }
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.save_btn -> {
+                    viewModel.onSaveClick()
+                    findNavController().popBackStack()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/com/example/gemspile/ui/add_video/AddVideoViewModel.kt
+++ app/src/main/java/com/example/gemspile/ui/add_video/AddVideoViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.gemspile.ui.add_video
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gemspile.raw_model.VideoRaw
+import com.example.gemspile.use_case.SaveVideo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class AddVideoViewModel @Inject constructor() : ViewModel()
+@HiltViewModel
+class AddVideoViewModel @Inject constructor(
+    private val saveVideo: SaveVideo
+) : ViewModel() {
+    var urlTextField = ""
+
+    fun onSaveClick() = viewModelScope.launch {
+        val videoRaw = VideoRaw(
+            url = urlTextField
+        )
+        saveVideo(video = videoRaw)
+    }
+}
