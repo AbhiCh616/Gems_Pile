6974ac7b82823292435a26e5beb8db4fc47de4c8
abhic <abhich1607@gmail.com>
2022-08-28 20:23:18 +0530

    Navigate to add video page on click on add video FAB

--- app/src/main/java/com/example/gemspile/ui/MainActivity.kt
+++ app/src/main/java/com/example/gemspile/ui/MainActivity.kt
@@ -1,22 +1,8 @@
 package com.example.gemspile.ui
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
 import com.example.gemspile.R
-import com.example.gemspile.ui.video_list.VideoListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.main_activity) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<VideoListFragment>(R.id.fragment_container_view)
-            }
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
--- app/src/main/java/com/example/gemspile/ui/video_list/VideoListFragment.kt
+++ app/src/main/java/com/example/gemspile/ui/video_list/VideoListFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gemspile.R
 import com.example.gemspile.databinding.VideoListBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -40,6 +42,14 @@ class VideoListFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addVideoButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_videoList_to_addVideo)
+        }
     }
 
     override fun onDestroy() {
